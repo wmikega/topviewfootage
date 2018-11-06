b20e65adf244e2b230464f9abdf51cceafbde8de
Michael Walters <mwalters12@gsu.edu>
2018-11-05 19:57:24 -0500

    corrected theme name

--- gulpfile.js
+++ gulpfile.js
@@ -11,38 +11,38 @@ var pngquant = require('imagemin-pngquant');
 
 
 gulp.task('imagemin', function () {
-    return gulp.src('./themes/custom/endymion/images/*')
+    return gulp.src('./themes/custom/mountain/images/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         }))
-        .pipe(gulp.dest('./themes/custom/endymion/images'));
+        .pipe(gulp.dest('./themes/custom/mountain/images'));
 });
 
 
 gulp.task('sass', function () {
-  gulp.src('./themes/custom/endymion/sass/**/*.scss')
+  gulp.src('./themes/custom/mountain/sass/**/*.scss')
     .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./themes/custom/endymion/css'));
+    .pipe(gulp.dest('./themes/custom/mountain/css'));
 });
 
 
 gulp.task('uglify', function() {
-  gulp.src('./themes/custom/endymion/lib/*.js')
+  gulp.src('./themes/custom/mountain/lib/*.js')
     .pipe(uglify('main.js'))
-    .pipe(gulp.dest('./themes/custom/endymion/js'))
+    .pipe(gulp.dest('./themes/custom/mountain/js'))
 });
 
 gulp.task('watch', function(){
     livereload.listen();
 
-    gulp.watch('./themes/custom/endymion/sass/**/*.scss', ['sass']);
-    gulp.watch('./themes/custom/endymion/lib/*.js', ['uglify']);
-    gulp.watch(['./themes/custom/endymion/css/style.css', './themes/custom/endymion/**/*.twig', './themes/custom/endymion/js/*.js'], function (files){
+    gulp.watch('./themes/custom/mountain/sass/**/*.scss', ['sass']);
+    gulp.watch('./themes/custom/mountain/lib/*.js', ['uglify']);
+    gulp.watch(['./themes/custom/mountain/css/style.css', './themes/custom/mountain/**/*.twig', './themes/custom/mountain/js/*.js'], function (files){
         livereload.changed(files)
     });
 });
